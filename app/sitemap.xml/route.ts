1c969df925ac1f751749900a6461517cdd8d4084
naman290896 <naman290896@rediffmail.com>
2026-01-23 18:07:15 +0530

    Added sitemap

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const baseUrl = "https://www.renusafoods.com";
+
+  const urls = [
+    "",
+    "/about",
+    "/products",
+    "/contact",
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (path) => `
+  <url>
+    <loc>${baseUrl}${path}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`
+  )
+  .join("")}
+</urlset>`;
+
+  return new NextResponse(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
